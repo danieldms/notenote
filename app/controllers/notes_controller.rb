ff60b39022478582dca1fa3d2e860b447baf9887
daniel marques <daniel.dms.23@gmail.com>
2015-12-10 21:35:37 -0300

    Git add notes view

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,15 +1,28 @@
 class NotesController < ApplicationController
 
+	before_action :find_note, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@notes = Note.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
 	def new
+		@note = Note.new
 	end
 
 	def create
+		@note = Note.new(note_params)
+
+		if @note.save
+			flash[:notice] = "Note is create successfully!"
+			redirect_to @note
+		else
+			flash[:alert] = "It's not possible create a New Note. Try again!"
+			render "new"
+		end
 	end
 
 	def edit
@@ -24,6 +37,7 @@ class NotesController < ApplicationController
 	private
 		
 		def find_note
+			@note = Note.find(params[:id])
 		end
 
 		def note_params
